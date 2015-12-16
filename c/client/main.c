234c29ffbb1bb1ed68e2dd199e2236fc26ac25fd
joakimthun <joakimthun@gmail.com>
2015-12-16 22:37:36 +0100

    Added sdl

--- c/client/main.c
+++ c/client/main.c
@@ -1,8 +1,13 @@
+#include <SDL.h>
 #include <stdio.h>
 
 int main(int argc, char* argv[])
 {
-	printf("MMO Client");
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		printf("SDL could not be initialized! SDL_Error: %s\n", SDL_GetError());
+		return 1;
+	}
 
 	int c = getchar();
 
